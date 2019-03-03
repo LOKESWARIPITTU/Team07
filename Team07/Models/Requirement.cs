e65e52e4c01fdfda526eddd206a4120532d8e020
Abhinay <abhinayreddy1278@gmail.com>
2019-03-03 16:26:32 -0600

    commit

---
+++ Team07/Models/Requirement.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Team07.Models
+{
+    public class Requirement
+    {
+        public int ID { get; set; }
+        public string RequirementAbbrev { get; set; }
+        public string CourseName { get; set; }
+
+    }
+}
