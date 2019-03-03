b21af642882006d04a39f1bd9820c5a5c9c7c4a5
Lokeswari Pittu <s531378@nwmissouri.edu>
2019-03-03 14:59:16 -0600

    commit

---
+++ Team07/Models/Degree.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Team07.Models
+{
+    public class Degree
+    {
+        public int ID { get; set; }
+        public string DegreeAbbrev { get; set; }
+        public string DegreeName { get; set; }
+      
+
+    }
+}
