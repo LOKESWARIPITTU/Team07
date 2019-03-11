730a1f53ec1570353b85ced313f2825022c47489
Shivani Reddy Dodla <dodlashivanireddy@gmail.com>
2019-03-11 14:39:56 -0500

    commit

---
+++ Team07/Controllers/DegreeRequirementsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Team07.Data;
+using Team07.Models;
+
+namespace Team07.Controllers
+{
+    public class DegreeRequirementsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DegreeRequirementsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DegreeRequirements
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DegreeRequirements.ToListAsync());
+        }
+
+        // GET: DegreeRequirements/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var degreeRequirement = await _context.DegreeRequirements
+                .FirstOrDefaultAsync(m => m.DegreeRequirementId == id);
+            if (degreeRequirement == null)
+            {
+                return NotFound();
+            }
+
+            return View(degreeRequirement);
+        }
+
+        // GET: DegreeRequirements/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DegreeRequirements/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DegreeRequirementId,DegreeId,RequirementId")] DegreeRequirement degreeRequirement)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(degreeRequirement);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(degreeRequirement);
+        }
+
+        // GET: DegreeRequirements/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var degreeRequirement = await _context.DegreeRequirements.FindAsync(id);
+            if (degreeRequirement == null)
+            {
+                return NotFound();
+            }
+            return View(degreeRequirement);
+        }
+
+        // POST: DegreeRequirements/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DegreeRequirementId,DegreeId,RequirementId")] DegreeRequirement degreeRequirement)
+        {
+            if (id != degreeRequirement.DegreeRequirementId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(degreeRequirement);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DegreeRequirementExists(degreeRequirement.DegreeRequirementId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(degreeRequirement);
+        }
+
+        // GET: DegreeRequirements/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var degreeRequirement = await _context.DegreeRequirements
+                .FirstOrDefaultAsync(m => m.DegreeRequirementId == id);
+            if (degreeRequirement == null)
+            {
+                return NotFound();
+            }
+
+            return View(degreeRequirement);
+        }
+
+        // POST: DegreeRequirements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var degreeRequirement = await _context.DegreeRequirements.FindAsync(id);
+            _context.DegreeRequirements.Remove(degreeRequirement);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DegreeRequirementExists(int id)
+        {
+            return _context.DegreeRequirements.Any(e => e.DegreeRequirementId == id);
+        }
+    }
+}
