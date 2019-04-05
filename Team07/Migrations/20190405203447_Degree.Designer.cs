5f1de4b3232a595784c891b605df47bb61fe44b2
Lokeswari Pittu <s531378@nwmissouri.edu>
2019-04-05 15:35:14 -0500

    cmt

--- Team07/Migrations/20190405203447_Degree.Designer.cs
+++ Team07/Migrations/20190405203447_Degree.Designer.cs
@@ -10,7 +10,7 @@ using Team07.Data;
 namespace Team07.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190405202026_Degree")]
+    [Migration("20190405203447_Degree")]
     partial class Degree
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
