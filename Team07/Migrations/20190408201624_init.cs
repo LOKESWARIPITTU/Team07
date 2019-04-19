bfb846cdf2b04af9976a169625f8dc4da283f9c3
Rakesh217 <sairakesh217@gmail.com>
2019-04-19 14:47:25 -0500

    commiting

---
+++ Team07/Migrations/20190408201624_init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Team07.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Song",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Given = table.Column<string>(nullable: true),
+                    Family = table.Column<string>(nullable: true),
+                    GraduationTerm = table.Column<string>(maxLength: 150, nullable: false),
+                    DesiredPosition = table.Column<string>(maxLength: 150, nullable: false),
+                    Skills = table.Column<string>(nullable: true),
+                    Rating = table.Column<int>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Song", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Song");
+        }
+    }
+}
